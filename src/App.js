c092e955e2e0b07879b55e90e86c99fc0095669d
Lera <valeriya.mihaleva.1999@gmail.com>
2019-11-21 18:10:10 +0200

    Added New, List, Active components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,80 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { createContext, useState } from 'react';
+import { Active } from './components/Active';
+import { List } from './components/List';
+import { New } from './components/New';
+import { StyledButton } from './AppStyled';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { createGlobalStyle, ThemeProvider } from 'styled-components';
+
+const GlobalStyled = createGlobalStyle`
+  body {
+    mrgin: none;
+  }
+`;
+
+export const MyContext = createContext(null);
+
+const theme = {
+  primary: '#D1004B',
+  white: '#fff',
+  fontSizeRegular: '14px',
+  fontSizeBig: '16px'
 }
+
+const initialState = {
+  list: [
+    { id: 1, name: 'task 1', description: 'task 1 description' },
+    { id: 2, name: 'task 2', description: 'task 2 description' }
+  ],
+  active: null,
+  showNew: false
+}
+
+export const App = () => {
+  const [state, setState] = useState(initialState);
+  const getById = (taskId) => state.list.find(item => item.id === taskId);
+  const deleteTask = (taskId) => {
+     const newList = state.list.filter(item => item.id !== taskId);
+    setState({ ...state, list: newList, active: null });
+  }
+  const changeTask = (task) => {
+    const newTasks = state.list.map(t => {
+      if (t.id === task.id) {
+        return task;
+      }
+      return t;
+    })
+    setState({ ...state, list: newTasks, active: task })
+  }
+  const onShowNewTastForm = () => {
+    setState({ ...state, showNew: true, active: null })
+  }
+
+  const context = {
+    list: state.list
+  }
+
+  return (
+    <MyContext.Provider value={context}>
+    <ThemeProvider theme={theme}>
+    <div>
+    <GlobalStyled />
+    {
+      state.active && (
+        <Active />
+      )
+    }
+    {
+      state.showNew && (
+        <New />
+      )
+    }
+    <List />
+    <StyledButton onClick={onShowNewTastForm}>Add new task</StyledButton>
+    </div>
+    </ThemeProvider>
+    </MyContext.Provider>
+  )
+} 
 
 export default App;
